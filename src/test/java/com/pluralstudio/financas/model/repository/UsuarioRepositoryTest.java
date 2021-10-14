07fd12dcf61a2d6215616c5bfc07c8c50e01f41d
mcv <mcv@pop-os.localdomain>
2021-10-14 11:14:04 -0300

    Foi adicionado o profile de testes e o método para testar se não existir o email

--- src/test/java/com/pluralstudio/financas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/pluralstudio/financas/model/repository/UsuarioRepositoryTest.java
@@ -1,17 +1,17 @@
 package com.pluralstudio.financas.model.repository;
 
 import com.pluralstudio.financas.model.entities.Usuario;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 
     @Autowired
@@ -29,6 +29,18 @@ public class UsuarioRepositoryTest {
 
         //verificação
         Assertions.assertThat(result).isTrue();
+    }
+
+    @Test
+    public void deveRetornarFalsoQuandoNãoHouverUsuarioCadastradoComOEmail(){
+        //cenário
+        repository.deleteAll();
+
+        //ação
+        boolean result = repository.existsByEmail("usuario@email.com");
+
+        //verificação
+        Assertions.assertThat(result).isFalse();
     }
 
 
