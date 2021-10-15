1487c345ba02715a349573a0f4a558b4a2f2e73d
mcv <mcv@pop-os.localdomain>
2021-10-15 17:29:04 -0300

    Finalização dos endpoints de lançamento

--- src/main/java/com/pluralstudio/financas/resource/LancamentoResource.java
+++ src/main/java/com/pluralstudio/financas/resource/LancamentoResource.java
@@ -13,6 +13,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/lancamento")
 @AllArgsConstructor
@@ -82,6 +85,32 @@ public class LancamentoResource {
             }
         }).orElseGet(() ->
                 new ResponseEntity("Lançamento não encontrado na base de dados.",HttpStatus.BAD_REQUEST));
+
+    }
+
+    @GetMapping
+    public ResponseEntity buscar(
+            @RequestParam(value = "descricao",required = false) String descricao,
+            @RequestParam(value = "mes",required = false) Integer mes,
+            @RequestParam(value = "ano",required = false) Integer ano,
+            @RequestParam("usuario") Long idUsuario){
+        Lancamento lancamentoFiltro = new Lancamento();
+        lancamentoFiltro.setDescricao(descricao);
+        lancamentoFiltro.setMes(mes);
+        lancamentoFiltro.setAno(ano);
+
+        Optional<Usuario> usuario = usuarioService.obterUsuarioPorId(idUsuario);
+        if(usuario.isPresent()){
+            return ResponseEntity
+                    .badRequest()
+                    .body("Não foi possível realizar a consulta.Usuário não encontrado para o Id informado");
+        }
+        else {
+            lancamentoFiltro.setUsuario(usuario.get());
+        }
+
+        List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
+        return ResponseEntity.ok(lancamentos);
 
     }
 }
